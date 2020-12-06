f493979bcdde6782bb1233c362f7fac3727681e0
yutatanomoki <yuta3071@gmail.com>
2020-12-06 14:26:35 +0000

    [Add]flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,36 @@
 class BooksController < ApplicationController
   def top
+  end
   
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def new
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+        flash[:notice] = 'Book was successfully created.' 
+        redirect_to book_path(@book)
+    else
+        @books = Book.all
+        @book = Book.new
+        render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  #get 'top' => 'books#top'
+  root to: 'books#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
 end
