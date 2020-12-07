de274549ff50d2f1b6ee5c6e4f29d552a99bdcd6
yutatanomoki <yuta3071@gmail.com>
2020-12-07 13:33:26 +0000

    [Check]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,11 +15,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-        flash[:notice] = 'Book was successfully created.' 
-        redirect_to book_path(@book)
+      redirect_to book_path(@book) ,notice: 'Book was successfully created.'
     else
         @books = Book.all
-        @book = Book.new
         render :index
     end
   end
@@ -27,7 +25,24 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+    redirect_to book_path(book),notice: 'Book was successfully updated.'
+    else
+        #@books = Book.all
+        @book = Book.find(params[:id])
+        render :edit
+    end
+  end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
   private
   # ストロングパラメータ
   def book_params
